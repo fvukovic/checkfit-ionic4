a9e645f04641d0da138188fe75f017932f32223a
Filip Vukovic <filip.vukovic@ecx.io>
2020-04-06 23:31:00 +0200

    the map is added and it is working

--- src/app/pages/customer-homepage/customer-homepage.page.ts
+++ src/app/pages/customer-homepage/customer-homepage.page.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, AfterContentInit, ViewChild } from '@angular/core';
+declare var google;
 @Component({
   selector: 'app-customer-homepage',
   templateUrl: './customer-homepage.page.html',
   styleUrls: ['./customer-homepage.page.scss'],
 })
-export class CustomerHomepagePage implements OnInit {
+export class CustomerHomepagePage implements OnInit,AfterContentInit {
+   map; 
+  @ViewChild("mapElement", { static: true }) mapElement;
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  ngAfterContentInit(): void {
+    this.map = new google.maps.Map(
+        this.mapElement.nativeElement,
+        {
+          center: {lat: -34.397, lng: 150.644},
+          zoom: 8
+        });
+  }
 }
